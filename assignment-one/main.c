0bd2f65fbc7b3281c041755ab786fbcb0ae01b88
Sean Willison <sean.willison@gmail.com>
2018-09-06 15:14:04 -0700

    working on getting basic program runnong

--- assignment-one/main.c
+++ assignment-one/main.c
@@ -1,5 +1,17 @@
+/* C Example */
+#include <mpi.h>
 #include <stdio.h>
-int main( int argc, const char* argv[] )
+ 
+int main (int argc, char* argv[])
 {
-	printf( "\nHello World\n\n" );
+  int rank, size;
+ 
+  MPI_Init (&argc, &argv);      /* starts MPI */
+  MPI_Comm_rank (MPI_COMM_WORLD, &rank);        /* get current process id */
+  MPI_Comm_size (MPI_COMM_WORLD, &size);        /* get number of processes */
+  printf( "Hello world from process %d of %d\n", rank, size );
+  MPI_Finalize();
+  return 0;
 }
+
+// https://jetcracker.wordpress.com/2012/03/01/how-to-install-mpi-in-ubuntu/
