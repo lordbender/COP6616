efecb37fd0c409b6a0db5aaee24898e81270345a
Sean Willison <sean.willison@gmail.com>
2018-10-02 19:37:33 -0400

    rando

--- assignment-one/base-program/common.h
+++ assignment-one/base-program/common.h
@@ -4,3 +4,5 @@
 #define arrayLength(array) (sizeof((array)) / sizeof((array)[0]))
 
 void printArray(int arr[], int size);
+
+int* bubbleSort(int numbers[], int count);
--- assignment-one/base-program/main.c
+++ assignment-one/base-program/main.c
@@ -25,6 +25,10 @@ int main(int argc, char *argv[])
     my_array[i] = num;
   }
 
+  // Sort the Array
+  int *r = bubbleSort(my_array, arg1);
+
+  // Print the Array
   printArray(my_array, arg1);
 
   return 0;
