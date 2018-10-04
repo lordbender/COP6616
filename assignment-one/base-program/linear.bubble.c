085d7e07f7ecca9edde15c1bf441d4097c2298a7
Sean Willison <sean.willison@gmail.com>
2018-10-04 17:30:48 -0400

    correcting invalid array terminator

---
+++ assignment-one/base-program/linear.bubble.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+// A function to implement bubble sort
+int* bubbleSort(int numbers[], int count)
+{
+    int input, i, j, c;
+
+    for (i = 0; i < count - 1; i++)
+    {
+        for (j = 0; j < count - 1 - 1; j++)
+        {
+            if (numbers[j] > numbers[j + 1])
+            {
+                c = numbers[j];
+                numbers[j] = numbers[j + 1];
+                numbers[j + 1] = c;
+            }
+        }
+    }
+
+    return numbers;
+}
--- assignment-one/base-program/shared.c
+++ assignment-one/base-program/shared.c
@@ -4,7 +4,7 @@
 void printArray(int arr[], int size)
 {
     int i;
-    for (i = 0; i < size; i++)
+    for (i = 0; i < size - 1; i++)
         printf("\t%d\n ", arr[i]);
     printf("n");
 }
