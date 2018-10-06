3af0b12fa109ee9c4900dc6dbc15d99f690925db
Sean Willison <sean.willison@gmail.com>
2018-10-06 18:23:08 -0400

    Working on mpi bubble

--- assignment-one/mpi-program-bubble/mpi.bubble.c
+++ assignment-one/mpi-program-bubble/mpi.bubble.c
@@ -11,29 +11,14 @@ double run_mpi_bubble(int p, int id, int size, bool print)
     if (id == 0)
     {
         int bubble_m_out[size];
-        int *bubble_sort_a = create_one_d_matrix(size, bubble_m_out, print);
+        bubble_sort_a = create_one_d_matrix(size, bubble_m_out, print);
     }
 
-    // Print the Array - If you want!
-    if (print == true)
-    {
-        //  printArray(array, size);
-    }
+    double time_to_run = run_sort(p, id, size, bubble_sort_a, print);
 
-    MPI_Finalize();
-
-    return 0.0;
+    return time_to_run;
 }
 
-/* 
-   bsort.c - Parallel sorting algorithm based on bubblesort
-
-   compile: mpicc -Wall -O -o bsort bsort.c
-   run:     mpirun -np num_procs bsort in_file out_file
-*/
-
-/* swap entries in array v at positions i and j; used by bubblesort */
-/* this improves performance; Exercise: by how much? */
 static inline void swap(int *v, int i, int j)
 {
     int t = v[i];
@@ -41,7 +26,6 @@ static inline void swap(int *v, int i, int j)
     v[j] = t;
 }
 
-/* (bubble) sort array v; array is of length n */
 void bubblesort(int *v, int n)
 {
     int i, j;
@@ -51,7 +35,6 @@ void bubblesort(int *v, int n)
                 swap(v, j, j + 1);
 }
 
-/* merge two sorted arrays v1, v2 of lengths n1, n2, respectively */
 int *merge(int *v1, int n1, int *v2, int n2)
 {
     int *result = (int *)malloc((n1 + n2) * sizeof(int));
@@ -71,12 +54,12 @@ int *merge(int *v1, int n1, int *v2, int n2)
             i++;
         }
         else if (v1[i] < v2[j])
-        { // indices in bounds as i < n1 && j < n2
+        {
             result[k] = v1[i];
             i++;
         }
         else
-        { // v2[j] <= v1[i]
+        {
             result[k] = v2[j];
             j++;
         }
@@ -84,113 +67,86 @@ int *merge(int *v1, int n1, int *v2, int n2)
     return result;
 }
 
-// int main(int argc, char **argv)
-// {
-//     int n;
-//     int *data = NULL;
-//     int c, s;
-//     int *chunk;
-//     int o;
-//     int *other;
-//     int step;
-//     int p, id;
-//     MPI_Status status;
-//     double elapsed_time;
-//     FILE *file = NULL;
-//     int i;
+double run_sort(int p, int id, int size, int *data, bool print)
+{
+    int n;
+    int c, s;
+    int *chunk;
+    int o;
+    int *other;
+    int step;
+    MPI_Status status;
+    double elapsed_time;
+    FILE *file = NULL;
+    int i;
 
-//     if (argc != 3)
-//     {
-//         fprintf(stderr, "Usage: mpirun -np <num_procs> %s <in_file> <out_file>\n", argv[0]);
-//         exit(1);
-//     }
+    if (id == 0)
+    {
+        c = n / p;
+        if (n % p)
+            c++;
+    }
 
-//     MPI_Init(&argc, &argv);
-//     MPI_Comm_size(MPI_COMM_WORLD, &p);
-//     MPI_Comm_rank(MPI_COMM_WORLD, &id);
+    // start the timer
+    MPI_Barrier(MPI_COMM_WORLD);
+    elapsed_time = -MPI_Wtime();
 
-//     if (id == 0)
-//     {
-//         // read size of data
-//         file = fopen(argv[1], "r");
-//         fscanf(file, "%d", &n);
-//         // compute chunk size
-//         c = n / p;
-//         if (n % p)
-//             c++;
-//         // read data from file
-//         data = (int *)malloc(p * c * sizeof(int));
-//         for (i = 0; i < n; i++)
-//             fscanf(file, "%d", &(data[i]));
-//         fclose(file);
-//         // pad data with 0 -- doesn't matter
-//         for (i = n; i < p * c; i++)
-//             data[i] = 0;
-//     }
+    // broadcast size
+    MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-//     // start the timer
-//     MPI_Barrier(MPI_COMM_WORLD);
-//     elapsed_time = -MPI_Wtime();
+    // compute chunk size
+    c = n / p;
+    if (n % p)
+        c++;
 
-//     // broadcast size
-//     MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    // scatter data
+    chunk = (int *)malloc(c * sizeof(int));
+    MPI_Scatter(data, c, MPI_INT, chunk, c, MPI_INT, 0, MPI_COMM_WORLD);
 
-//     // compute chunk size
-//     c = n / p;
-//     if (n % p)
-//         c++;
+    // compute size of own chunk and sort it
+    s = (n >= c * (id + 1)) ? c : n - c * id;
+    bubblesort(chunk, s);
 
-//     // scatter data
-//     chunk = (int *)malloc(c * sizeof(int));
-//     MPI_Scatter(data, c, MPI_INT, chunk, c, MPI_INT, 0, MPI_COMM_WORLD);
-//     free(data);
-//     data = NULL;
+    // up to log_2 p merge steps
+    for (step = 1; step < p; step = 2 * step)
+    {
+        if (id % (2 * step) != 0)
+        {
+            // id is no multiple of 2*step: send chunk to id-step and exit loop
+            MPI_Send(chunk, s, MPI_INT, id - step, 0, MPI_COMM_WORLD);
+            break;
+        }
+        // id is multiple of 2*step: merge in chunk from id+step (if it exists)
+        if (id + step < p)
+        {
+            // compute size of chunk to be received
+            o = (n >= c * (id + 2 * step)) ? c * step : n - c * (id + step);
+            // receive other chunk
+            other = (int *)malloc(o * sizeof(int));
+            MPI_Recv(other, o, MPI_INT, id + step, 0, MPI_COMM_WORLD, &status);
+            // merge and free memory
+            data = merge(chunk, s, other, o);
+            free(chunk);
+            free(other);
+            chunk = data;
+            s = s + o;
+        }
+    }
 
-//     // compute size of own chunk and sort it
-//     s = (n >= c * (id + 1)) ? c : n - c * id;
-//     bubblesort(chunk, s);
+    // stop the timer
+    elapsed_time += MPI_Wtime();
 
-//     // up to log_2 p merge steps
-//     for (step = 1; step < p; step = 2 * step)
-//     {
-//         if (id % (2 * step) != 0)
-//         {
-//             // id is no multiple of 2*step: send chunk to id-step and exit loop
-//             MPI_Send(chunk, s, MPI_INT, id - step, 0, MPI_COMM_WORLD);
-//             break;
-//         }
-//         // id is multiple of 2*step: merge in chunk from id+step (if it exists)
-//         if (id + step < p)
-//         {
-//             // compute size of chunk to be received
-//             o = (n >= c * (id + 2 * step)) ? c * step : n - c * (id + step);
-//             // receive other chunk
-//             other = (int *)malloc(o * sizeof(int));
-//             MPI_Recv(other, o, MPI_INT, id + step, 0, MPI_COMM_WORLD, &status);
-//             // merge and free memory
-//             data = merge(chunk, s, other, o);
-//             free(chunk);
-//             free(other);
-//             chunk = data;
-//             s = s + o;
-//         }
-//     }
+    // write sorted data to out file and print out timer
+    if (id == 0)
+    {
+    }
 
-//     // stop the timer
-//     elapsed_time += MPI_Wtime();
+    // Print the Array - If you want!
+    if (id == 0 && print == true)
+    {
+        printf("Bubblesort %d ints on %d procs: %f secs\n", n, p, elapsed_time);
+        printArray(data, size);
+    }
 
-//     // write sorted data to out file and print out timer
-//     if (id == 0)
-//     {
-//         file = fopen(argv[2], "w");
-//         fprintf(file, "%d\n", s); // assert (s == n)
-//         for (i = 0; i < s; i++)
-//             fprintf(file, "%d\n", chunk[i]);
-//         fclose(file);
-//         printf("Bubblesort %d ints on %d procs: %f secs\n", n, p, elapsed_time);
-//         // printf("%d %2d %f\n", n, p, elapsed_time);
-//     }
-
-//     MPI_Finalize();
-//     return 0;
-// }
+    return elapsed_time;
+}
