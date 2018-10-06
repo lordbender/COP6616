8b71d9f7178b92f44e4294887cdf35ac03895de6
Sean Willison <sean.willison@gmail.com>
2018-10-06 13:59:50 -0400

    linear portion of the program is complete

--- assignment-one/base-program/main.c
+++ assignment-one/base-program/main.c
@@ -28,12 +28,19 @@ int main(int argc, char *argv[])
   // Linear -> O(n^2) -> Bubble Sort Opperations
   int bubble_m_out[size];
   int *bubble_sort_a = create_one_d_matrix(size, bubble_m_out, false);
-  double cpu_time_used = run_linear_bubble(bubble_sort_a, size, false);
-  printf("Linear -> O(n^2) -> Bubble Sort on %d integers, took %f\n\n", size, cpu_time_used);
+  double cpu_time_used_bubble_sort = run_linear_bubble(bubble_sort_a, size, false);
+  printf("Linear -> O(n^2) -> Bubble Sort on %d integers, took %f\n\n", size, cpu_time_used_bubble_sort);
 
   // Linear -> O(n^3) -> Matrix Multipication
   double cpu_time_used_multiply_array = run_linear_matrix_multiply(size, false);
   printf("Linear -> O(n^3) -> Multiply Matrix on %d X %d integers, took %f\n\n", size, size, cpu_time_used_multiply_array);
+
+  // Create Report Here!
+  printf("\n\n\nReport of Linear performance for %d as the specified size:\n\n", size);
+  printf("\t\tO(1)   ran in: %f!\n", cpu_time_used_o_of_one);
+  printf("\t\tO(n)   ran in: %f!\n", cpu_time_used_linear_search);
+  printf("\t\tO(n^2) ran in: %f!\n", cpu_time_used_bubble_sort);
+  printf("\t\tO(n^3) ran in: %f!\n", cpu_time_used_multiply_array);
 
   return 0;
 }
