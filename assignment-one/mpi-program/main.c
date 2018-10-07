b5308d70cf91e79a3030937f58f65663cb5a378a
Sean Willison <sean.willison@gmail.com>
2018-10-07 11:13:20 -0400

    working on a better understanding of MPI

--- assignment-one/mpi-program/main.c
+++ assignment-one/mpi-program/main.c
@@ -2,9 +2,35 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdbool.h>
+#include <mpi.h>
 
 int main(int argc, char *argv[])
 {
-    printf("Place Holcer for running ALL MPI programs.\n\n");
+    int p, id;
+
+    // MPI Stuff
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: mpirun -np <num_procs> %s <random_array_size>\n", argv[0]);
+        printf("You did not feed me arguments, I will die now :( ...\n");
+        exit(1);
+    }
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &p);
+    MPI_Comm_rank(MPI_COMM_WORLD, &id);
+
+    // Grab the Requested Size from the Command Line Arguments.
+    int size = atoi(argv[1]);
+
+    //Define process 0 behavior
+    if (id == 0)
+    {
+        printf("Size => %d, created by %d\n", size, id);
+    }
+
+    printf("Echo ID => %d\n", id);
+
+    MPI_Finalize();
     return 0;
 }
