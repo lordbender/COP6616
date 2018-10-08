a9019f127fc7bcd1f867c74fa0d3b8a89dd8d43e
Sean Willison <sean.willison@gmail.com>
2018-10-08 18:47:35 -0400

    happy search done.

--- assignment-one/mpi-program/common.h
+++ assignment-one/mpi-program/common.h
@@ -8,6 +8,9 @@
 #define UPPER 10000
 #define LOWER -10000
 
+// Programs
+void run_search(int my_process_id, int number_of_processess, int size);
+
 // Shared Functions
 long *create_one_d_matrix(int size, bool printOutput);
 void printArray(long arr[], int size);
