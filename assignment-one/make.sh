b75e998a090362531ba74eb74ffbca8183c28eb3
Sean Willison <sean.willison@gmail.com>
2018-10-13 14:04:09 -0400

    getting reporting in order

--- assignment-one/make.sh
+++ assignment-one/make.sh
@@ -7,6 +7,7 @@ echo Running Linear All
 pushd base-program
 chmod 777 run.sh
 ./run.sh $SIZE 
+cp report.linear.txt ../report.linear.txt
 popd
 
 echo Running MPI Bubble
@@ -18,5 +19,6 @@ popd
 echo Running MPI Matrix
 pushd mpi-program-matrix
 chmod 777 run.sh
-./run.sh $PROCS 500
+./run.sh
+cp report.mpi-matrix.txt ../report.mpi-matrix.txt
 popd
