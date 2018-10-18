a183c957f32f3f16fd89be928aced2b9c9913e10
Sean Willison <sean.willison@bcbsfl.com>
2018-10-18 09:25:03 -0400

    working on parell matrix mult

--- assignment-two/thread-management/services/MatrixTools.java
+++ assignment-two/thread-management/services/MatrixTools.java
@@ -5,6 +5,17 @@ import java.util.Random;
 
 public class MatrixTools {
 
+    public void runTimedExperiment(int size) {
+        long[][] a = getTestArray(size);
+        long[][] b = getTestArray(size);
+
+        long startTime = System.nanoTime();
+        long[][] c = this.multiply(a, b);
+        long endTime = System.nanoTime();
+
+        System.out.println("Timed Run: " + endTime - start);
+    }
+
     public void test() {
         long[][] arr = getTestArray(5);
         for (int i = 0; i < 5; i++) {
@@ -28,8 +39,18 @@ public class MatrixTools {
         return outter;
     }
 
-    public long[][] multiply(long[][] a, long[][] b) {
+    private long[][] multiply(long[][] a, long[][] b) {
         long c[][] = new long[a.length][a.length];
+
+        // Citation:
+        // https://www.sanfoundry.com/java-program-perform-matrix-multiplication/
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size; j++)
+                for (int k = 0; k < size; k++) {
+                    c[i][j] = c[i][j] + a[i][k] * b[k][j];
+                }
+
+        return c;
     }
 
 }
