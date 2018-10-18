0ea9c262444a1647789e69e9229765fd138220df
Sean Willison <sean.willison@gmail.com>
2018-10-18 19:41:20 -0400

    meh

---
+++ assignment-two/Dotnet.Tpl.Example/Services/MatrixService.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Dotnet.Tpl.Example.Services
+{
+    public class MatrixServices
+    {
+        public void RunTest(int size)
+        {
+            Console.WriteLine(size);
+        }
+
+    }
+}
