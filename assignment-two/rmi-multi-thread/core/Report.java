00ebae897899277ea93c8515c11ce5a2632dfd18
Sean Willison <sean.willison@gmail.com>
2018-10-22 19:19:01 -0400

    Completing O(n^2) testing

--- assignment-two/rmi-multi-thread/core/Report.java
+++ assignment-two/rmi-multi-thread/core/Report.java
@@ -4,7 +4,7 @@ import models.ReportModel;
 
 public class Report {
     public static void create(ReportModel model) {
-        System.out.println("\n\n\tLinear: " + model.getAlgorythmName());
+        System.out.println("\n\n\tRMI: " + model.getAlgorythmName());
         System.out.println("\t\tComplexity      : " + model.getComplexity());
         System.out.println("\t\tExecution Time  : " + model.getDuration());
         System.out.print("\n-----------------------------------------\n\n");
--- assignment-two/rmi-multi-thread/services/BubbleSort.java
+++ assignment-two/rmi-multi-thread/services/BubbleSort.java
@@ -35,6 +35,11 @@ public class BubbleSort {
     class BubbleSortTask implements Runnable {
         private int size = 0;
         private long arr[];
+        private long sorted[];
+
+        public long[] getSorted() {
+            return this.sorted;
+        }
 
         BubbleSortTask(long[] a) {
             this.arr = a;
@@ -57,18 +62,100 @@ public class BubbleSort {
                 t.start();
             }
 
+            long[][] results = new long[POOL_SIZE][chunkSize];
             for (int i = 0; i < POOL_SIZE; i++) {
                 // As it implements Future, we can call get()
                 try {
                     long[] result = (long[]) futureTasks[i].get();
+                    results[i] = result;
 
                     // Merge the sorted arrays...
 
                 } catch (Exception e) {
                 }
+            }
 
+            // Bring all of the arrays back together
+            // or merge all of the arrays.
+            long[] c = new long[this.size];
+            for (int i = 0; i < POOL_SIZE; i++) {
+                for (int j = 0; j < chunkSize; j++) {
+                    c[i * j] = results[i][j];
+                }
+            }
+
+            // n^2 operations done - mergesort now...
+            this.sort(c, 0, c.length - 1);
+
+            this.sorted = c;
+        }
+
+        // Citation
+        // https://www.geeksforgeeks.org/merge-sort/
+        private void sort(long arr[], int l, int r) {
+            if (l < r) {
+                // Find the middle point
+                int m = (l + r) / 2;
+
+                // Sort first and second halves
+                sort(arr, l, m);
+                sort(arr, m + 1, r);
+
+                // Merge the sorted halves
+                merge(arr, l, m, r);
             }
         }
+
+        // Citation
+        // https://www.geeksforgeeks.org/merge-sort/
+        private void merge(long arr[], int l, int m, int r) {
+            // Find sizes of two subarrays to be merged
+            int n1 = m - l + 1;
+            int n2 = r - m;
+
+            /* Create temp arrays */
+            long L[] = new long[n1];
+            long R[] = new long[n2];
+
+            /* Copy data to temp arrays */
+            for (int i = 0; i < n1; ++i)
+                L[i] = arr[l + i];
+            for (int j = 0; j < n2; ++j)
+                R[j] = arr[m + 1 + j];
+
+            /* Merge the temp arrays */
+
+            // Initial indexes of first and second subarrays
+            int i = 0, j = 0;
+
+            // Initial index of merged subarry array
+            int k = l;
+            while (i < n1 && j < n2) {
+                if (L[i] <= R[j]) {
+                    arr[k] = L[i];
+                    i++;
+                } else {
+                    arr[k] = R[j];
+                    j++;
+                }
+                k++;
+            }
+
+            /* Copy remaining elements of L[] if any */
+            while (i < n1) {
+                arr[k] = L[i];
+                i++;
+                k++;
+            }
+
+            /* Copy remaining elements of R[] if any */
+            while (j < n2) {
+                arr[k] = R[j];
+                j++;
+                k++;
+            }
+        }
+
     }
 
     class CallableSort implements Callable {
