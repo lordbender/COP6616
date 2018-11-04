6531e8f898735ba5e593b1b9f9c5c0853af3ca58
Sean Willison <sean.willison@gmail.com>
2018-11-04 17:30:34 -0500

    working on bubble sort

---
+++ assignment-three/go/bubble.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func bubbleSort() {
+	const size = 100000
+	var array = getArray(size)
+
+	start := time.Now()
+	for i := 0; i < size; i++ {
+		for j := 0; j < size-i-1; j++ {
+
+			if array[j] > array[j+1] {
+				array[j], array[j+1] = array[j+1], array[j]
+			}
+		}
+	}
+	elapsed := time.Since(start)
+	fmt.Printf("\tSorted %d numbers in %s seconds\n", size, elapsed)
+}
