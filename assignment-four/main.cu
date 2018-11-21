401b6031587ad35c70776e55a75d6014b793780a
Sean Willison <sean.willison@gmail.com>
2018-11-21 14:51:21 -0500

    seg fault mess

--- assignment-four/main.cu
+++ assignment-four/main.cu
@@ -1,17 +1,7 @@
 #include <stdio.h>
 #include "main_cuda.h"
 
-#define N 1000
-
-#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
-inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
-{
-   if (code != cudaSuccess) 
-   {
-      fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
-      if (abort) exit(code);
-   }
-}
+#define N 16
 
 // Device code
 __global__ void VecSquare(float* A, float* C, int size)
@@ -24,29 +14,31 @@ __global__ void VecSquare(float* A, float* C, int size)
 // Host code
 int main()
 {
-
 	printf("Starting on size %d!!!\n", N);
 
-    float* h_A = (float*)malloc(N);
-    float* h_C = (float*)malloc(N);
+    float* host_A = (float*)malloc(N);
+    float* host_C = (float*)malloc(N);
 
 	for (int i = 0; i < N; i++)
     {
-		h_A[i] = (float)(i + i);
-		h_C[i] = 0.0;
+		host_A[i] = (float)(i + i);
+		host_C[i] = 0.0;
 	}
+
 	printf("Working Arrays Created\n");
 
-    float* d_A;
-	gpuErrchk(cudaMalloc(&d_A, N));
-	
-	float* d_C;
-	gpuErrchk(cudaMalloc(&d_C, N));
+    float* device_A;
+	cudaMalloc((void**)&device_A, 1000);
+	printf("Cuda device_A Memory Allocated\n");
+
+	float* device_C;
+	cudaMalloc((void**)&device_C, N);
+	printf("Cuda device_C Memory Allocated\n");
 
 	printf("Cuda Memory Allocated\n");
 
     // Copy vector from host memory to device memory
-    gpuErrchk(cudaMemcpy(d_A, h_A, N, cudaMemcpyHostToDevice));
+    cudaMemcpy(device_A, host_A, N, cudaMemcpyHostToDevice);
 
 	printf("Cuda Data Copy Completed\n");
 
@@ -55,25 +47,23 @@ int main()
     int blocksPerGrid =
 			(N + threadsPerBlock - 1) / threadsPerBlock;
 			
-    VecSquare<<<blocksPerGrid, threadsPerBlock>>>(d_A, d_C, N);
+    VecSquare<<<blocksPerGrid, threadsPerBlock>>>(device_A, device_C, N);
 
-	gpuErrchk( cudaPeekAtLastError() );
-	gpuErrchk( cudaDeviceSynchronize() );
+	cudaPeekAtLastError();
+	cudaDeviceSynchronize();
 
-	cudaMemcpy(h_C, d_C, N, cudaMemcpyDeviceToHost);
+	cudaMemcpy(host_C, device_C, N, cudaMemcpyDeviceToHost);
 	
-
-
     // Free device memory
-    cudaFree(d_A);
-	cudaFree(d_C);
+    cudaFree(device_A);
+	cudaFree(device_C);
 	
  	for (int i = 0; i < N; i++){
-		printf("\tCool Story %f\n", h_C[i]);
+		printf("\tCool Story %f\n", host_C[i]);
 	}
 
-	free(h_A);
-	free(h_C);
+	free(host_A);
+	free(host_C);
 	
 	printf("Done!!!\n");
 }
