3884ab23744072fbe5c00f3be984ba6fad2c1d98
Sean Willison <sean.willison@gmail.com>
2018-11-21 17:51:58 -0500

    working on cuda basics

--- assignment-four/main.cu
+++ assignment-four/main.cu
@@ -1,25 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 #include "main_cuda.h"
 
-#define N 16
+static const int BLOCK_SIZE = 256;
+static const int N = 10;
 
 // Device code
-__global__ void VecSquare(float* A, float* C, int size)
+__global__ void vecSquare(float* a, float* c, int n)
 {
-    int i = blockDim.x * blockIdx.x + threadIdx.x;
-    if (i < size)
-        C[i] = A[i] * A[i];
+	int id = blockIdx.x * blockDim.x + threadIdx.x;
+    if (id < n)
+        c[id] = (float)a[id] + (float)5.0;
 }
             
 // Host code
 int main()
 {
-	size_t size = N;
-
 	printf("Starting on size %d!!!\n", N);
 
-    float* host_A = (float*)malloc(N);
-    float* host_C = (float*)malloc(N);
+    // float* host_A = (float*)malloc(N * sizeof(float*));
+    // float* host_C = (float*)malloc(N* sizeof(float*));
+
+	float* host_A = new float[N];
+	float* host_C = new float[N];
 
 	for (int i = 0; i < N; i++)
     {
@@ -30,42 +34,47 @@ int main()
 	printf("Working Arrays Created\n");
 
     float* device_A;
-	cudaError_t cudaMalloc(void** &device_A, size);
+	cudaMalloc((void**) &device_A, sizeof(float) * N);
 	printf("Cuda device_A Memory Allocated\n");
 
-	// float* device_C;
-	// cudaMalloc((void**)&device_C, N);
-	// printf("Cuda device_C Memory Allocated\n");
+	gpuErrchk(cudaGetLastError());
 
-	// printf("Cuda Memory Allocated\n");
+	float* device_C;
+	cudaMalloc((void**) &device_C, sizeof(float) * N);
+	printf("Cuda device_C Memory Allocated\n");
 
-    // // Copy vector from host memory to device memory
-    // cudaMemcpy(device_A, host_A, N, cudaMemcpyHostToDevice);
+	gpuErrchk(cudaGetLastError());
+	
+	printf("Cuda Memory Allocated\n");
 
-	// printf("Cuda Data Copy Completed\n");
+    // Copy vector from host memory to device memory
+    cudaMemcpy(device_A, host_A, N, cudaMemcpyHostToDevice);
 
-    // // Invoke kernel
-    // int threadsPerBlock = 256;
-    // int blocksPerGrid =
-	// 		(N + threadsPerBlock - 1) / threadsPerBlock;
-			
-    // VecSquare<<<blocksPerGrid, threadsPerBlock>>>(device_A, device_C, N);
+	printf("Cuda Data Copy Completed\n");
 
-	// cudaPeekAtLastError();
-	// cudaDeviceSynchronize();
+    // Invoke kernel
+    int grid = ceil(N * 1.0 / BLOCK_SIZE);
+    vecSquare<<<grid, BLOCK_SIZE>>>(device_A, device_C, N);
 
-	// cudaMemcpy(host_C, device_C, N, cudaMemcpyDeviceToHost);
+	cudaDeviceSynchronize();
+
+	gpuErrchk(cudaGetLastError());
+
+	// Copy the result back from the device to the host.
+	cudaMemcpy(host_C, device_C, sizeof(float) * N, cudaMemcpyDeviceToHost);
 	
     // Free device memory
     cudaFree(device_A);
-	// cudaFree(device_C);
+	cudaFree(device_C);
 	
- 	// for (int i = 0; i < N; i++){
-	// 	printf("\tCool Story %f\n", host_C[i]);
-	// }
+ 	for (int i = 0; i < N; i++) {
+		printf("\tCool Story %f\n", host_C[i]);
+	}
 
 	free(host_A);
 	free(host_C);
 	
-	printf("Done!!!\n");
+	cudaDeviceReset();
+
+	printf("Done!!!\n\n");
 }
