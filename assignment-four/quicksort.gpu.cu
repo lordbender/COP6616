4697aac7d7ac5b1a312fdcfe6bf375430e0a6746
Sean Willison <sean.willison@gmail.com>
2018-11-23 13:55:46 -0500

    working on cuda recursion

--- assignment-four/quicksort.gpu.cu
+++ assignment-four/quicksort.gpu.cu
@@ -6,49 +6,57 @@
 
 static const int BLOCK_SIZE = 256;
 
-__device__ void swap_device(int *a, int* b) 
-{ 
-	int t = *a; 
-	*a = *b; 
-	*b = t; 
-} 
-
-__device__ int partition_device (int *arr, int low, int high) 
-{ 
-	int pivot = arr[high];
-	int i = (low - 1);
-
-	for (int j = low; j <= high- 1; j++) 
-	{ 
-		if (arr[j] <= pivot) 
-		{ 
-			i++;
-			swap_device(&arr[i], &arr[j]); 
-		} 
-	} 
-	swap_device(&arr[i + 1], &arr[high]); 
-	return (i + 1); 
-} 
-
-__global__ void quicksort_device(int *arr, int low, int high)
+__device__ void swap_device(int *a, int *b)
 {
-	cudaStream_t s1, s2;
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
 
-	if (low < high){
-		int pi = partition_device(arr, low, high); 
+__device__ int partition_device(int *arr, int low, int high)
+{
+    int pivot = arr[high];
+    int i = (low - 1);
 
-		cudaStreamCreateWithFlags(&s1, cudaStreamNonBlocking);
-		cudaStreamCreateWithFlags(&s2, cudaStreamNonBlocking);
+    for (int j = low; j <= high - 1; j++)
+    {
+        if (arr[j] <= pivot)
+        {
+            i++;
+            swap_device(&arr[i], &arr[j]);
+        }
+    }
+    swap_device(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
 
-		quicksort_device<<<1, 64, 0, s1>>>(arr, low, pi - 1);
-		quicksort_device<<<1, 64, 0, s2>>>(arr, pi + 1, high);
-	}
-	return;
+__global__ void quicksort_device(int *data, int left, int right)
+{
+    cudaStream_t s1, s2;
+
+    int pi = partition_device(data, left, right);
+
+    int nright = pi - 1;
+    int nleft = pi + 1;
+
+    if (left < nright)
+    {
+        cudaStreamCreateWithFlags(&s1, cudaStreamNonBlocking);
+        quicksort_device<<<1, 64, 0, s1>>>(data, left, nright);
+    }
+
+    if (nleft < right)
+    {
+        cudaStreamCreateWithFlags(&s2, cudaStreamNonBlocking);
+        quicksort_device<<<1, 64, 0, s2>>>(data, nleft, right);
+    }
+
+    return;
 }
 
 double quicksort_gpu(int size)
 {
-	int *ha, *da;
+    int *ha, *da;
 
     ha = (int *)malloc(sizeof(int) * size);
 
@@ -65,9 +73,10 @@ double quicksort_gpu(int size)
     gpuErrchk(cudaMemcpy(da, ha, sizeof(int) * size, cudaMemcpyHostToDevice));
     gpuErrchk(cudaGetLastError());
 
-	int grid = ceil(size * 1.0 / BLOCK_SIZE);
-	
+    int grid = ceil(size * 1.0 / BLOCK_SIZE);
+
     quicksort_device<<<grid, BLOCK_SIZE>>>(da, 0, size - 1);
+
     cudaDeviceSynchronize();
     gpuErrchk(cudaGetLastError());
 
