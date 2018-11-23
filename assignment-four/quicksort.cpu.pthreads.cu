c8943878607b90735016b538de7eb75587845884
Sean Willison <sean.willison@gmail.com>
2018-11-23 16:18:39 -0500

    working on understanding how to parallelize quick sort

--- assignment-four/quicksort.cpu.pthreads.cu
+++ assignment-four/quicksort.cpu.pthreads.cu
@@ -38,11 +38,11 @@ double quicksort_cpu_pthreads(int size)
     thread th2(thread_obj(), 3); 
   
     // Define a Lambda Expression 
-    // auto f = [](int x) { 
-    //     for (int i = 0; i < x; i++) 
-    //         cout << "Thread using lambda"
-    //          " expression as callable\n"; 
-    // }; 
+    auto f = [](int x) { 
+        for (int i = 0; i < x; i++) 
+            cout << "Thread using lambda"
+             " expression as callable\n"; 
+    }; 
   
     thread th3(f, 3); 
   
