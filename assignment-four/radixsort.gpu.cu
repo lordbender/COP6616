770c4cf5ece5a96f4932a7f073d85e3b07055401
Sean Willison <sean.willison@gmail.com>
2018-11-24 00:07:43 -0500

    working on radix

--- assignment-four/radixsort.gpu.cu
+++ assignment-four/radixsort.gpu.cu
@@ -83,10 +83,10 @@ duration<double> radixsort_gpu(int size)
     high_resolution_clock::time_point end = high_resolution_clock::now();
 
     // Testing that sort is working, keep commented out on large values of N (say N > 1000)
-    for (int i = 0; i < size; i++)
-    {
-        printf("\t %d\n", hc[i]);
-    }
+    // for (int i = 0; i < size; i++)
+    // {
+    //     printf("\t %d\n", hc[i]);
+    // }
 
     free(ha);
     free(hc);
