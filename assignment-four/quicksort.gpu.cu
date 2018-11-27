d10809d3fe4cc89f6f9dc6c1d594a6afca7a69c6
Sean Willison <sean.willison@gmail.com>
2018-11-27 10:59:39 -0500

    testing

--- assignment-four/quicksort.gpu.cu
+++ assignment-four/quicksort.gpu.cu
@@ -70,26 +70,60 @@ __global__ void quicksort_device(int *data, int left, int right, int depth)
         selection_sort(data, left, right);
         return;
     }
-        
-    int pi = partition_device(data, left, right);
 
-    int nright = pi - 1;
-    int nleft = pi + 1;
+    int *lptr = data+left;
+    int *rptr = data+right;
+    int  pivot = data[(left+right)/2];
 
-    if (left < nright)
+    // Do the partitioning.
+    while (lptr <= rptr)
+    {
+        // Find the next left- and right-hand values to swap
+        int lval = *lptr;
+        int rval = *rptr;
+
+        // Move the left pointer as long as the pointed element is smaller than the pivot.
+        while (lval < pivot)
+        {
+            lptr++;
+            lval = *lptr;
+        }
+
+        // Move the right pointer as long as the pointed element is larger than the pivot.
+        while (rval > pivot)
+        {
+            rptr--;
+            rval = *rptr;
+        }
+
+        // If the swap points are valid, do the swap!
+        if (lptr <= rptr)
+        {
+            *lptr++ = rval;
+            *rptr-- = lval;
+        }
+    }
+
+    // Now the recursive part
+    int nright = rptr - data;
+    int nleft  = lptr - data;
+
+    // Launch a new block to sort the left part.
+    if (left < (rptr-data))
+    {
+        cudaStream_t s;
+        cudaStreamCreateWithFlags(&s, cudaStreamNonBlocking);
+        quicksort_device<<< 1, 1, 0, s >>>(data, left, nright, depth+1);
+        cudaStreamDestroy(s);
+    }
+
+    // Launch a new block to sort the right part.
+    if ((lptr-data) < right)
     {
         cudaStream_t s1;
         cudaStreamCreateWithFlags(&s1, cudaStreamNonBlocking);
-        quicksort_device<<<1, 64, 0, s1>>>(data, left, nright, depth + 1);
+        quicksort_device<<< 1, 1, 0, s1 >>>(data, nleft, right, depth+1);
         cudaStreamDestroy(s1);
-    }
-
-    if (nleft < right)
-    {
-        cudaStream_t s2;
-        cudaStreamCreateWithFlags(&s2, cudaStreamNonBlocking);
-        quicksort_device<<<1, 64, 0, s2>>>(data, nleft, right, depth + 1);
-        cudaStreamDestroy(s2);
     }
 }
 
