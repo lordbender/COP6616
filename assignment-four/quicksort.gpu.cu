04cafda7df844a6c33a682a806f7a86974c40f04
Sean Willison <sean.willison@gmail.com>
2018-11-27 18:17:52 -0500

    testing

--- assignment-four/quicksort.gpu.cu
+++ assignment-four/quicksort.gpu.cu
@@ -7,57 +7,149 @@
 
 static const int BLOCK_SIZE = 256;
 
-__device__ void swap_device(int *a, int *b)
-{
-    int t = *a;
-    *a = *b;
-    *b = t;
-}
+#define MAX_DEPTH       16
+#define INSERTION_SORT  32
 
-__device__ int partition_device(int *arr, int low, int high)
+__device__ void selection_sort(int *data, int left, int right)
 {
-    int pivot = arr[high];
-    int i = (low - 1);
-
-    for (int j = low; j <= high - 1; j++)
+    for (int i = left ; i <= right ; ++i)
     {
-        if (arr[j] <= pivot)
+        int min_val = data[i];
+        int min_idx = i;
+
+        // Find the smallest value in the range [left, right].
+        for (int j = i+1 ; j <= right ; ++j)
         {
-            i++;
-            swap_device(&arr[i], &arr[j]);
+            int val_j = data[j];
+
+            if (val_j < min_val)
+            {
+                min_idx = j;
+                min_val = val_j;
+            }
+        }
+
+        // Swap the values.
+        if (i != min_idx)
+        {
+            data[min_idx] = data[i];
+            data[i] = min_val;
         }
     }
-    swap_device(&arr[i + 1], &arr[high]);
-    return (i + 1);
 }
 
-// Based on CUDA Examples - But Optimized
-__global__ void quicksort_device(int *data, int left, int right)
-{ 
-    cudaStream_t s1, s2;
-    int pi = partition_device(data, left, right);
-
-    int nright = pi - 1;
-    int nleft = pi + 1;
-
-    if (left < nright)
+__global__ void cdp_simple_quicksort(int *data, int left, int right, int depth)
+{
+    if (depth >= MAX_DEPTH || right-left <= INSERTION_SORT)
     {
-        // cudaStreamCreateWithFlags(&s1, cudaStreamNonBlocking);
-        // quicksort_device<<<1, 32, 0, s1>>>(data, left, nright);
+        selection_sort(data, left, right);
+        return;
     }
 
-    if (nleft < right)
+    int *lptr = data+left;
+    int *rptr = data+right;
+    int  pivot = data[(left+right)/2];
+
+    while (lptr <= rptr)
     {
-        // cudaStreamCreateWithFlags(&s2, cudaStreamNonBlocking);
-        // quicksort_device<<<1, 32, 0, s2>>>(data, nleft, right);
+        int lval = *lptr;
+        int rval = *rptr;
+
+        while (lval < pivot)
+        {
+            lptr++;
+            lval = *lptr;
+        }
+
+        // Move the right pointer as long as the pointed element is larger than the pivot.
+        while (rval > pivot)
+        {
+            rptr--;
+            rval = *rptr;
+        }
+
+        // If the swap points are valid, do the swap!
+        if (lptr <= rptr)
+        {
+            *lptr++ = rval;
+            *rptr-- = lval;
+        }
+    }
+
+    // Now the recursive part
+    int nright = rptr - data;
+    int nleft  = lptr - data;
+
+    // Launch a new block to sort the left part.
+    if (left < (rptr-data))
+    {
+        cudaStream_t s;
+        cudaStreamCreateWithFlags(&s, cudaStreamNonBlocking);
+        cdp_simple_quicksort<<< 1, 1, 0, s >>>(data, left, nright, depth+1);
+        cudaStreamDestroy(s);
+    }
+
+    // Launch a new block to sort the right part.
+    if ((lptr-data) < right)
+    {
+        cudaStream_t s1;
+        cudaStreamCreateWithFlags(&s1, cudaStreamNonBlocking);
+        cdp_simple_quicksort<<< 1, 1, 0, s1 >>>(data, nleft, right, depth+1);
+        cudaStreamDestroy(s1);
     }
 }
+
+// __device__ void swap_device(int *a, int *b)
+// {
+//     int t = *a;
+//     *a = *b;
+//     *b = t;
+// }
+
+// __device__ int partition_device(int *arr, int low, int high)
+// {
+//     int pivot = arr[high];
+//     int i = (low - 1);
+
+//     for (int j = low; j <= high - 1; j++)
+//     {
+//         if (arr[j] <= pivot)
+//         {
+//             i++;
+//             swap_device(&arr[i], &arr[j]);
+//         }
+//     }
+//     swap_device(&arr[i + 1], &arr[high]);
+//     return (i + 1);
+// }
+
+// // Based on CUDA Examples - But Optimized
+// __global__ void quicksort_device(int *data, int left, int right)
+// { 
+//     cudaStream_t s1, s2;
+//     int pi = partition_device(data, left, right);
+
+//     int nright = pi - 1;
+//     int nleft = pi + 1;
+
+//     if (left < nright)
+//     {
+//         // cudaStreamCreateWithFlags(&s1, cudaStreamNonBlocking);
+//         // quicksort_device<<<1, 32, 0, s1>>>(data, left, nright);
+//     }
+
+//     if (nleft < right)
+//     {
+//         // cudaStreamCreateWithFlags(&s2, cudaStreamNonBlocking);
+//         // quicksort_device<<<1, 32, 0, s2>>>(data, nleft, right);
+//     }
+// }
 
 void quicksort_host(int *da, int *hc, int size)
 {
     // Call the device.
     int grid = ceil(size * 1.0 / BLOCK_SIZE);
-    quicksort_device<<<grid, BLOCK_SIZE>>>(da, 0, size - 1);
+    cdp_simple_quicksort<<<grid, BLOCK_SIZE>>>(da, 0, size - 1, MAX_DEPTH);
     gpuErrchk(cudaGetLastError());
 
     // Ensure the Device is in sync, before we copy the data back!
