1679092826e8af171b39fb90b7efdfe6065313f5
Sean Willison <sean.willison@gmail.com>
2018-11-27 19:45:43 -0500

    seperating c++ and cuda

--- assignment-four/nvcc/quicksort.example.cu
+++ assignment-four/nvcc/quicksort.example.cu
@@ -1,167 +1,200 @@
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <ctime>
-// #include <ratio>
-// #include <chrono>
-// #include <iostream>
-// #include <cstdio>
+#include <iostream>
+#include <cstdio>
+#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
+{
+   if (code != cudaSuccess) 
+   {
+      fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+      if (abort) exit(code);
+   }
+}
 
-// #include "main_cuda.cuh"
+#define MAX_DEPTH       16
+#define INSERTION_SORT  32
 
-// #define MAX_DEPTH       16
-// #define INSERTION_SORT  32
+__device__ void selection_sort(unsigned int *data, int left, int right)
+{
+    for (int i = left ; i <= right ; ++i)
+    {
+        unsigned min_val = data[i];
+        int min_idx = i;
 
-// __device__ void selection_sort(unsigned int *data, int left, int right)
-// {
-//     for (int i = left ; i <= right ; ++i)
-//     {
-//         unsigned min_val = data[i];
-//         int min_idx = i;
+        // Find the smallest value in the range [left, right].
+        for (int j = i+1 ; j <= right ; ++j)
+        {
+            unsigned val_j = data[j];
 
-//         // Find the smallest value in the range [left, right].
-//         for (int j = i+1 ; j <= right ; ++j)
-//         {
-//             unsigned val_j = data[j];
+            if (val_j < min_val)
+            {
+                min_idx = j;
+                min_val = val_j;
+            }
+        }
 
-//             if (val_j < min_val)
-//             {
-//                 min_idx = j;
-//                 min_val = val_j;
-//             }
-//         }
+        // Swap the values.
+        if (i != min_idx)
+        {
+            data[min_idx] = data[i];
+            data[i] = min_val;
+        }
+    }
+}
 
-//         // Swap the values.
-//         if (i != min_idx)
-//         {
-//             data[min_idx] = data[i];
-//             data[i] = min_val;
-//         }
-//     }
-// }
+////////////////////////////////////////////////////////////////////////////////
+// Very basic quicksort algorithm, recursively launching the next level.
+////////////////////////////////////////////////////////////////////////////////
+__global__ void cdp_simple_quicksort(unsigned int *data, int left, int right, int depth)
+{
+    // If we're too deep or there are few elements left, we use an insertion sort...
+    if (depth >= MAX_DEPTH || right-left <= INSERTION_SORT)
+    {
+        selection_sort(data, left, right);
+        return;
+    }
 
-// __global__ void cdp_simple_quicksort(unsigned int *data, int left, int right, int depth)
-// {
-//     if (depth >= MAX_DEPTH || right-left <= INSERTION_SORT)
-//     {
-//         selection_sort(data, left, right);
-//         return;
-//     }
+    unsigned int *lptr = data+left;
+    unsigned int *rptr = data+right;
+    unsigned int  pivot = data[(left+right)/2];
 
-//     unsigned int *lptr = data+left;
-//     unsigned int *rptr = data+right;
-//     unsigned int  pivot = data[(left+right)/2];
+    // Do the partitioning.
+    while (lptr <= rptr)
+    {
+        // Find the next left- and right-hand values to swap
+        unsigned int lval = *lptr;
+        unsigned int rval = *rptr;
 
-//     while (lptr <= rptr)
-//     {
-//         unsigned int lval = *lptr;
-//         unsigned int rval = *rptr;
+        // Move the left pointer as long as the pointed element is smaller than the pivot.
+        while (lval < pivot)
+        {
+            lptr++;
+            lval = *lptr;
+        }
 
-//         while (lval < pivot)
-//         {
-//             lptr++;
-//             lval = *lptr;
-//         }
+        // Move the right pointer as long as the pointed element is larger than the pivot.
+        while (rval > pivot)
+        {
+            rptr--;
+            rval = *rptr;
+        }
 
-//         // Move the right pointer as long as the pointed element is larger than the pivot.
-//         while (rval > pivot)
-//         {
-//             rptr--;
-//             rval = *rptr;
-//         }
+        // If the swap points are valid, do the swap!
+        if (lptr <= rptr)
+        {
+            *lptr++ = rval;
+            *rptr-- = lval;
+        }
+    }
 
-//         // If the swap points are valid, do the swap!
-//         if (lptr <= rptr)
-//         {
-//             *lptr++ = rval;
-//             *rptr-- = lval;
-//         }
-//     }
+    // Now the recursive part
+    int nright = rptr - data;
+    int nleft  = lptr - data;
 
-//     // Now the recursive part
-//     int nright = rptr - data;
-//     int nleft  = lptr - data;
+    // Launch a new block to sort the left part.
+    if (left < (rptr-data))
+    {
+        cudaStream_t s;
+        cudaStreamCreateWithFlags(&s, cudaStreamNonBlocking);
+        cdp_simple_quicksort<<< 1, 1, 0, s >>>(data, left, nright, depth+1);
+        cudaStreamDestroy(s);
+    }
 
-//     // Launch a new block to sort the left part.
-//     if (left < (rptr-data))
-//     {
-//         cudaStream_t s;
-//         cudaStreamCreateWithFlags(&s, cudaStreamNonBlocking);
-//         cdp_simple_quicksort<<< 1, 1, 0, s >>>(data, left, nright, depth+1);
-//         cudaStreamDestroy(s);
-//     }
+    // Launch a new block to sort the right part.
+    if ((lptr-data) < right)
+    {
+        cudaStream_t s1;
+        cudaStreamCreateWithFlags(&s1, cudaStreamNonBlocking);
+        cdp_simple_quicksort<<< 1, 1, 0, s1 >>>(data, nleft, right, depth+1);
+        cudaStreamDestroy(s1);
+    }
+}
 
-//     // Launch a new block to sort the right part.
-//     if ((lptr-data) < right)
-//     {
-//         cudaStream_t s1;
-//         cudaStreamCreateWithFlags(&s1, cudaStreamNonBlocking);
-//         cdp_simple_quicksort<<< 1, 1, 0, s1 >>>(data, nleft, right, depth+1);
-//         cudaStreamDestroy(s1);
-//     }
-// }
+////////////////////////////////////////////////////////////////////////////////
+// Call the quicksort kernel from the host.
+////////////////////////////////////////////////////////////////////////////////
+void run_qsort(unsigned int *data, unsigned int nitems)
+{
+    // Prepare CDP for the max depth 'MAX_DEPTH'.
+    gpuErrchk(cudaDeviceSetLimit(cudaLimitDevRuntimeSyncDepth, MAX_DEPTH));
 
-// ////////////////////////////////////////////////////////////////////////////////
-// // Call the quicksort kernel from the host.
-// ////////////////////////////////////////////////////////////////////////////////
-// void run_qsort(unsigned int *data, unsigned int nitems)
-// {
-//     // Prepare CDP for the max depth 'MAX_DEPTH'.
-//     cudaDeviceSetLimit(cudaLimitDevRuntimeSyncDepth, MAX_DEPTH);
+    // Launch on device
+    int left = 0;
+    int right = nitems-1;
+    std::cout << "Launching kernel on the GPU" << std::endl;
+    cdp_simple_quicksort<<< 1, 1 >>>(data, left, right, 0);
+    gpuErrchk(cudaDeviceSynchronize());
+}
 
-//     // Launch on device
-//     int left = 0;
-//     int right = nitems-1;
-//     std::cout << "Launching kernel on the GPU" << std::endl;
-//     cdp_simple_quicksort<<< 1, 1 >>>(data, left, right, 0);
-//     cudaDeviceSynchronize();
-// }
+////////////////////////////////////////////////////////////////////////////////
+// Initialize data on the host.
+////////////////////////////////////////////////////////////////////////////////
+void initialize_data(unsigned int *dst, unsigned int nitems)
+{
+    // Fixed seed for illustration
+    srand(2047);
 
-// ////////////////////////////////////////////////////////////////////////////////
-// // Initialize data on the host.
-// ////////////////////////////////////////////////////////////////////////////////
-// void initialize_data(unsigned int *dst, unsigned int nitems)
-// {
-//     // Fixed seed for illustration
-//     srand(2047);
+    // Fill dst with random values
+    for (unsigned i = 0 ; i < nitems ; i++)
+        dst[i] = rand() % nitems ;
+}
 
-//     // Fill dst with random values
-//     for (unsigned i = 0 ; i < nitems ; i++)
-//         dst[i] = rand() % nitems ;
-// }
+////////////////////////////////////////////////////////////////////////////////
+// Verify the results.
+////////////////////////////////////////////////////////////////////////////////
+void check_results(int n, unsigned int *results_d)
+{
+    unsigned int *results_h = new unsigned[n];
+    gpuErrchk(cudaMemcpy(results_h, results_d, n*sizeof(unsigned), cudaMemcpyDeviceToHost));
 
+    for (int i = 1 ; i < n ; ++i)
+        if (results_h[i-1] > results_h[i])
+        {
+            std::cout << "Invalid item[" << i-1 << "]: " << results_h[i-1] << " greater than " << results_h[i] << std::endl;
+            exit(EXIT_FAILURE);
+        }
 
-// void check_results(int n, unsigned int *results_d)
-// {
-//     unsigned int *results_h = new unsigned[n];
-//     cudaMemcpy(results_h, results_d, n*sizeof(unsigned), cudaMemcpyDeviceToHost);
-//     for (int i = 0; i < n; i++)
-//     {
-//         printf("\t results_h[ %d ] => %d\n", i, results_h[i]);
-//     }
+    std::cout << "OK" << std::endl;
+    delete[] results_h;
+}
 
-//     delete[] results_h;
-// }
+////////////////////////////////////////////////////////////////////////////////
+// Main entry point.
+////////////////////////////////////////////////////////////////////////////////
+int main(int argc, char **argv)
+{
+    int num_items = 128;
+    bool verbose = true;
 
-// duration<double> quicksort_gpu_streams(int size)
-// {
-//     unsigned int *h_data = 0;
-//     unsigned int *d_data = 0;
+    // Create input data
+    unsigned int *h_data = 0;
+    unsigned int *d_data = 0;
 
-//     h_data =(unsigned int *)malloc(size*sizeof(unsigned int));
-//     initialize_data(h_data, size);
+    // Allocate CPU memory and initialize data.
+    std::cout << "Initializing data:" << std::endl;
+    h_data =(unsigned int *)malloc(num_items*sizeof(unsigned int));
+    initialize_data(h_data, num_items);
 
-//     high_resolution_clock::time_point start = high_resolution_clock::now();
-//     cudaMalloc((void **)&d_data, size * sizeof(unsigned int));
-//     cudaMemcpy(d_data, h_data, size * sizeof(unsigned int), cudaMemcpyHostToDevice);
+    if (verbose)
+    {
+        for (int i=0 ; i<num_items ; i++)
+            std::cout << "Data [" << i << "]: " << h_data[i] << std::endl;
+    }
 
-//     run_qsort(d_data, size);
+    // Allocate GPU memory.
+    gpuErrchk(cudaMalloc((void **)&d_data, num_items * sizeof(unsigned int)));
+    gpuErrchk(cudaMemcpy(d_data, h_data, num_items * sizeof(unsigned int), cudaMemcpyHostToDevice));
 
-//     check_results(size, d_data);
+    // Execute
+    std::cout << "Running quicksort on " << num_items << " elements" << std::endl;
+    run_qsort(d_data, num_items);
 
-//     free(h_data);
-//     cudaFree(d_data);
-//     high_resolution_clock::time_point end = high_resolution_clock::now();
+    // Check result
+    std::cout << "Validating results: ";
+    check_results(num_items, d_data);
 
-//     return time_calc(start, end);
-// }
+    free(h_data);
+    gpuErrchk(cudaFree(d_data));
+
+    exit(EXIT_SUCCESS);
+}
 
--- assignment-four/nvcc/quicksort.gpu.cu
+++ assignment-four/nvcc/quicksort.gpu.cu
@@ -1,119 +1,119 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
-inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
-{
-   if (code != cudaSuccess) 
-   {
-      fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
-      if (abort) exit(code);
-   }
-}
+// #define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+// inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
+// {
+//    if (code != cudaSuccess) 
+//    {
+//       fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+//       if (abort) exit(code);
+//    }
+// }
 
-static const int BLOCK_SIZE = 256;
+// static const int BLOCK_SIZE = 256;
 
-__device__ void swap_device(int *a, int *b)
-{
-    int t = *a;
-    *a = *b;
-    *b = t;
-}
+// __device__ void swap_device(int *a, int *b)
+// {
+//     int t = *a;
+//     *a = *b;
+//     *b = t;
+// }
 
-__device__ int partition_device(int *arr, int low, int high)
-{
-    int pivot = arr[high];
-    int i = (low - 1);
+// __device__ int partition_device(int *arr, int low, int high)
+// {
+//     int pivot = arr[high];
+//     int i = (low - 1);
 
-    for (int j = low; j <= high - 1; j++)
-    {
-        if (arr[j] <= pivot)
-        {
-            i++;
-            swap_device(&arr[i], &arr[j]);
-        }
-    }
-    swap_device(&arr[i + 1], &arr[high]);
-    return (i + 1);
-}
+//     for (int j = low; j <= high - 1; j++)
+//     {
+//         if (arr[j] <= pivot)
+//         {
+//             i++;
+//             swap_device(&arr[i], &arr[j]);
+//         }
+//     }
+//     swap_device(&arr[i + 1], &arr[high]);
+//     return (i + 1);
+// }
 
-// Based on CUDA Examples - But Optimized
-__global__ void quicksort_device(int *data, int left, int right)
-{ 
-    cudaStream_t s1, s2;
-    int pi = partition_device(data, left, right);
+// // Based on CUDA Examples - But Optimized
+// __global__ void quicksort_device(int *data, int left, int right)
+// { 
+//     cudaStream_t s1, s2;
+//     int pi = partition_device(data, left, right);
 
-    int nright = pi - 1;
-    int nleft = pi + 1;
+//     int nright = pi - 1;
+//     int nleft = pi + 1;
 
-    if (left < nright)
-    {
-        cudaStreamCreateWithFlags(&s1, cudaStreamNonBlocking);
-        quicksort_device<<<1, 32, 0, s1>>>(data, left, nright);
-    }
+//     if (left < nright)
+//     {
+//         cudaStreamCreateWithFlags(&s1, cudaStreamNonBlocking);
+//         quicksort_device<<<1, 32, 0, s1>>>(data, left, nright);
+//     }
 
-    if (nleft < right)
-    {
-        cudaStreamCreateWithFlags(&s2, cudaStreamNonBlocking);
-        quicksort_device<<<1, 32, 0, s2>>>(data, nleft, right);
-    }
-}
+//     if (nleft < right)
+//     {
+//         cudaStreamCreateWithFlags(&s2, cudaStreamNonBlocking);
+//         quicksort_device<<<1, 32, 0, s2>>>(data, nleft, right);
+//     }
+// }
 
-void quicksort_host(int *da, int *hc, int size)
-{
-    // Call the device.
-    int grid = ceil(size * 1.0 / BLOCK_SIZE);
-    quicksort_device<<<grid, BLOCK_SIZE>>>(da, 0, size - 1);
-    gpuErrchk(cudaGetLastError());
+// void quicksort_host(int *da, int *hc, int size)
+// {
+//     // Call the device.
+//     int grid = ceil(size * 1.0 / BLOCK_SIZE);
+//     quicksort_device<<<grid, BLOCK_SIZE>>>(da, 0, size - 1);
+//     gpuErrchk(cudaGetLastError());
 
-    // Ensure the Device is in sync, before we copy the data back!
-    gpuErrchk(cudaDeviceSynchronize());
-    gpuErrchk(cudaGetLastError());
+//     // Ensure the Device is in sync, before we copy the data back!
+//     gpuErrchk(cudaDeviceSynchronize());
+//     gpuErrchk(cudaGetLastError());
 
-    // Copy the results back from the device.
-    gpuErrchk(cudaMemcpy(hc, da, sizeof(int) * size, cudaMemcpyDeviceToHost));
-    gpuErrchk(cudaGetLastError());
+//     // Copy the results back from the device.
+//     gpuErrchk(cudaMemcpy(hc, da, sizeof(int) * size, cudaMemcpyDeviceToHost));
+//     gpuErrchk(cudaGetLastError());
 
-    // Testing that sort is working, keep commented out on large values of N (say N > 1000)
-    // for (int i = 0; i < size; i++)
-    // {
-    //     printf("\t %d\n", hc[i]);
-    // }
-}
+//     // Testing that sort is working, keep commented out on large values of N (say N > 1000)
+//     // for (int i = 0; i < size; i++)
+//     // {
+//     //     printf("\t %d\n", hc[i]);
+//     // }
+// }
 
-int main(int argc, char *argv[])
-{
-    cudaEvent_t start, stop;
-    cudaEventCreate(&start);
-    cudaEventCreate(&stop);
+// int main(int argc, char *argv[])
+// {
+//     cudaEvent_t start, stop;
+//     cudaEventCreate(&start);
+//     cudaEventCreate(&stop);
 
-  	srand(time(0));
+//   	srand(time(0));
 
- 	int size = atoi(argv[1]);
+//  	int size = atoi(argv[1]);
 
-    int* ha = (int *)malloc(sizeof(int) * size);
-    int* hc = (int *)malloc(sizeof(int) * size);
+//     int* ha = (int *)malloc(sizeof(int) * size);
+//     int* hc = (int *)malloc(sizeof(int) * size);
 
-    for (int i = 0; i < size; i++)
-    {
-        ha[i] = rand();
-        hc[i] = 0;
-    }
+//     for (int i = 0; i < size; i++)
+//     {
+//         ha[i] = rand();
+//         hc[i] = 0;
+//     }
 
-    int *da;
-    gpuErrchk(cudaMalloc((void **)&da, sizeof(int) * size));
-    gpuErrchk(cudaMemcpy(da, ha, sizeof(int) * size, cudaMemcpyHostToDevice));
-    gpuErrchk(cudaGetLastError());
+//     int *da;
+//     gpuErrchk(cudaMalloc((void **)&da, sizeof(int) * size));
+//     gpuErrchk(cudaMemcpy(da, ha, sizeof(int) * size, cudaMemcpyHostToDevice));
+//     gpuErrchk(cudaGetLastError());
 
-    // Kick off the sort!
-    quicksort_host(da, hc, size);
+//     // Kick off the sort!
+//     quicksort_host(da, hc, size);
 
-    gpuErrchk(cudaFree(da));
-    free(ha);
-    free(hc);
+//     gpuErrchk(cudaFree(da));
+//     free(ha);
+//     free(hc);
     
-    cudaEventSynchronize(stop);
-    float milliseconds = 0;
-    cudaEventElapsedTime(&milliseconds, start, stop);
+//     cudaEventSynchronize(stop);
+//     float milliseconds = 0;
+//     cudaEventElapsedTime(&milliseconds, start, stop);
 
-	printf("\tGPU O(n*log(n)) Streamed: Completed %d numbers in %f seconds!!!\n\n", size, milliseconds);
-}
+// 	printf("\tGPU O(n*log(n)) Streamed: Completed %d numbers in %f seconds!!!\n\n", size, milliseconds);
+// }
