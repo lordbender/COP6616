42b3452a736e30a68ad2b768f882f5c6071e2b6b
Sean Willison <sean.willison@gmail.com>
2018-11-28 10:45:48 -0500

    updating test

--- assignment-four/test.sh
+++ assignment-four/test.sh
@@ -1,13 +1,13 @@
 pushd cpp
 echo g++ -std=c++14 *.cpp -lpthread -o cpu.out
 g++ -std=c++11 *.cpp -lpthread -o cpu.out
-./cpu.out 1000000
+./cpu.out 100000
 popd
 
 
 pushd cuda
 echo nvcc -arch=compute_35 -rdc=true *.cu -o gpu.out
 nvcc -arch=compute_35 -rdc=true --default-stream per-thread *.cu -o gpu.out
-./gpu.out 1000000
+./gpu.out 100000
 popd
 
